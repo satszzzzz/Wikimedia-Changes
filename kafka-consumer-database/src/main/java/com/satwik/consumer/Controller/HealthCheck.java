30f3e4795c814a38cf77f1fe2976ec2287fc358b
Satwik Das <satwikdas872@gmail.com>
2025-07-02 00:56:06 +0530

    1.4:Creating jars and removing old jars (in target directories)

--- kafka-consumer-database/src/main/java/com/satwik/consumer/Controller/HealthCheck.java
+++ kafka-consumer-database/src/main/java/com/satwik/consumer/Controller/HealthCheck.java
@@ -1,5 +1,4 @@
 package com.satwik.consumer.Controller;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
